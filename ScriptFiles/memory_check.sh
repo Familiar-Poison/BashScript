1974d71fa8b7aac0fa0a936de704127b0a5c4ab3
Familiar-Poison <169057882+Familiar-Poison@users.noreply.github.com>
2024-05-23 14:17:26 +0800

    Update memory_check.sh

--- ScriptFiles/memory_check.sh
+++ ScriptFiles/memory_check.sh
@@ -8,28 +8,33 @@ error () {
 }
 
 usage () {
-    echo "Usage: $0 [-c critical threshold %] [-w warning threshold %] [-e email address]"
+
+    cat << EOF
+
+    Usage: $0 [OPTIONS]
+
+    Options:
+        -c      Critical threshold in %
+        -w      Warning threshold in %
+        -e      Email address to send the report
+EOF
+
+    # echo "Usage: ./$0 [-c critical threshold %] [-w warning threshold %] [-e email address]"
     exit 128
     #echo "Exit code $?"
 }
 
+email () {
 
-critical_exit () {
-
-    exit 2
+    current_datetime=$(date +'%Y%m%d %H:%M')
+    email_subject="$current_datetime memory_check - critical"
+    email_content=$(ps aux --sort=-%mem | awk '{print $1, $2, $4, $11}' | head -n 11 | column -t)
+    echo -e "Subject: $email_subject\n$email_content" | msmtp "$email_recipient"
 }
 
-warning_exit () {
-
-    exit 1
-}
-
-normal_exit () {
-
-    exit 0
-}
 
 main () {
+
     # free
     TOTAL_MEMORY=$( free | grep Mem: | awk '{ print $2 }')
     USED_MEMORY=$( free | grep Mem: | awk '{ print $3 }')
@@ -37,10 +42,6 @@ main () {
     
     USED_PERCENT=$(echo "scale=2; 100 * $USED_MEMORY / $TOTAL_MEMORY" | bc)
     FREE_PERCENT=$(echo "scale=2; 100 * $FREE_MEMORY / $TOTAL_MEMORY" | bc)
-
-    # echo $TOTAL_MEMORY
-    # echo $USED_PERCENT
-    # echo $FREE_PERCENT
 
     while getopts ":c:w:e:" opt; do
         case ${opt} in
@@ -51,7 +52,7 @@ main () {
                 WARNING_THRESHOLD=$OPTARG
                 ;;
             e )
-                email=$OPTARG
+                email_recipient=$OPTARG
                 ;;
             \? )
                 echo "Invalid option"
@@ -67,37 +68,47 @@ main () {
     # Argument checking
 
     if [ -z "$CRITICAL_THRESHOLD" ] || [ -z "$WARNING_THRESHOLD" ]; then
-        echo "Missing argument - critical and warning threshold"
+        echo "ERROR: Missing argument - Critical/Warning threshold"
         usage
 
     elif (( $(echo "$CRITICAL_THRESHOLD < 0 || $CRITICAL_THRESHOLD > 100" | bc -l) )); then
-        echo "Warning threshold must be within 0-100%."
+        echo "ERROR: Critical threshold must be within 0-100%."
         usage
 
     elif (( $(echo "$WARNING_THRESHOLD < 0 || $WARNING_THRESHOLD > 100" | bc -l) )); then
-        echo "Warning threshold must be within 0-100%."
+        echo "ERROR: Warning threshold must be within 0-100%."
         usage
 
     elif (( $(echo "$CRITICAL_THRESHOLD < $WARNING_THRESHOLD" | bc -l) )); then
-        echo "Critical threshold must be greater than warning threshold." 
+        echo "ERROR: Critical threshold must be greater than warning threshold." 
         usage
 
     fi
 
     # Main logic
 
-    if (( $(echo "$USED_PERCENT > $CRITICAL_THRESHOLD" | bc -l) )); then
-        echo "Critical threshold has been reached!"
-        critical_exit
-    
-    elif (( $(echo "$USED_PERCENT > $WARNING_THRESHOLD && $USED_PERCENT " | bc -l) )); then
-        echo "Critical threshold has been reached!"
-        warning_exit
+    echo "USED: $USED_PERCENT"
+    # echo "CRITICAL: $CRITICAL_PERCENT"
+    # echo "WARNING: $WARNING_PERCENT"
 
+    if (( $(echo "$USED_PERCENT >= $CRITICAL_THRESHOLD" | bc -l) )); then
+        echo "Critical threshold has been reached!"
+        email
+        exit 2
+    
+    elif (( $(echo "$USED_PERCENT >= $WARNING_THRESHOLD && $USED_PERCENT < $CRITICAL_THRESHOLD" | bc -l) )); then
+        echo "Warning threshold has been reached!"
+        exit 1
+
+    elif (( $(echo "$USED_PERCENT < $WARNING_THRESHOLD" | bc -l) )); then
+        echo "Usage is still within threshold"
+        exit 0
     fi    
 
+    
     
 }
 
 
 main "$@"
+# echo "Exit code $?"
